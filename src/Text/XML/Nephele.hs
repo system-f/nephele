66d9105cd7e8b78347aa669565b23006fa18580a
Tony Morris <tmorris@tmorris.net>
2013-10-28 22:03:19 +1000

    comment parsers

--- src/Text/XML/Nephele.hs
+++ src/Text/XML/Nephele.hs
@@ -1,17 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Text.XML.Nephele where
 
--- | Add two integers.
---
--- Examples:
---
--- >>> add 7 8
--- 15
---
--- prop> n `add` 0 == n
-add ::
-  Int
-  -> Int
-  -> Int
-add =
-  (+)
+import Text.Parser.Char
+import Data.Text(Text)
 
+-- $setup
+-- >>> import Text.Parsec
+-- >>> import Control.Lens
+-- >>> import Test.QuickCheck
+-- >>> import Prelude
+-- >>> newtype UncommentBegin = UncommentBegin String deriving (Eq, Show)
+-- >>> instance Arbitrary UncommentBegin where arbitrary = fmap UncommentBegin (arbitrary `suchThat` (/= "<!--"))
+-- >>> newtype UncommentEnd = UncommentEnd String deriving (Eq, Show)
+-- >>> instance Arbitrary UncommentEnd where arbitrary = fmap UncommentEnd (arbitrary `suchThat` (/= "-->"))
+
+-- | The parser for the beginning of a comment.
+--
+-- >>> parse commentBegin "test" "<!--"
+-- Right "<!--"
+--
+-- prop> \(UncommentBegin s) -> isn't _Right (parse commentBegin "test" s)
+commentBegin ::
+  CharParsing m =>
+  m Text
+commentBegin =
+  text "<!--"
+
+-- | The parser for the end of a comment.
+--
+-- >>> parse commentEnd "test" "-->"
+-- Right "-->"
+--
+-- prop> \(UncommentEnd s) -> isn't _Right (parse commentEnd "test" s)
+commentEnd ::
+  CharParsing m =>
+  m Text
+commentEnd =
+  text "-->"
