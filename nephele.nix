bb7130cd6bfa0e9ae68ba90c79ebf9c1a64fb98a
Tony Morris <tmorris@tmorris.net>
2018-04-17 11:08:43 +1000

    BaseChar

--- nephele.nix
+++ nephele.nix
@@ -1,17 +1,17 @@
 { mkDerivation, base, checkers, hedgehog, lens, papa, parsec
 , parsers, QuickCheck, semigroups, stdenv, tasty, tasty-hedgehog
-, tasty-hunit, tasty-quickcheck, text, transformers
+, tasty-hunit, tasty-quickcheck, text, text1, transformers
 }:
 mkDerivation {
   pname = "nephele";
   version = "0.0.1";
   src = ./.;
   libraryHaskellDepends = [
-    base lens papa parsec parsers semigroups text transformers
+    base lens papa parsec parsers semigroups text text1 transformers
   ];
   testHaskellDepends = [
     base checkers hedgehog lens papa parsec parsers QuickCheck tasty
-    tasty-hedgehog tasty-hunit tasty-quickcheck text
+    tasty-hedgehog tasty-hunit tasty-quickcheck text text1
   ];
   homepage = "https://github.com/qfpl/nephele";
   description = "General purpose XML parser and library";
